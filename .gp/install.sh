31441c0ce88b709d79475de0ad3587e328181b16
Felipe <felipe.alencar@iifal.edu.br>
2021-08-04 20:54:22 -0300

    - Alteração de estrutura do repositório; - Alteração de caminhos relativos.

--- .gp/install.sh
+++ .gp/install.sh
@@ -2,9 +2,9 @@ git clone --depth 1 https://github.com/phpmyadmin/phpmyadmin.git
 cd phpmyadmin
 composer update --no-dev
 yarn install
-cp ../../config.inc.php .
+cp ../config.inc.php .
 # copy AWS RDS CA bundle
-cp ../../rds-combined-ca-bundle.pem .
+cp ../rds-combined-ca-bundle.pem .
 
 echo "Done"
 source ~/.bashrc
