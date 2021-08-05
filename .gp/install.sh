925132c870e2dc1372182c23ba1c9022f16494b1
Felipe <felipe.alencar@iifal.edu.br>
2021-08-04 21:04:50 -0300

    - Ajuste de caminho.

--- .gp/install.sh
+++ .gp/install.sh
@@ -2,9 +2,9 @@ git clone --depth 1 https://github.com/phpmyadmin/phpmyadmin.git
 cd phpmyadmin
 composer update --no-dev
 yarn install
-cp ../config.inc.php .
+cp ../.gp/config.inc.php .
 # copy AWS RDS CA bundle
-cp ../rds-combined-ca-bundle.pem .
+cp ../.gp/rds-combined-ca-bundle.pem .
 
 echo "Done"
 
