2a42449042d7163cccde6c624ffe2d17fa92312a
Felipe <felipe.alencar@ifal.edu.br>
2021-08-13 23:11:46 -0300

    - Remoção de um merge equivocado.

--- php-basic/lec-13/banco.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE `pweb`;
-
-CREATE TABLE  `pweb`.`USUARIO` (
-  `ID_USUARIO` int(10) unsigned NOT NULL auto_increment,
-  `NOME` varchar(200) NOT NULL default '',
-  `EMAIL` varchar(200) NOT NULL default '',
-  `SENHA` varchar(45) NOT NULL default '',
-  PRIMARY KEY  (`ID_USUARIO`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
