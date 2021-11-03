602a190bcdd091077d92d7a62741c83ee3f8e5db
Felipe <felipe.alencar@ifal.edu.br>
2021-11-03 10:54:42 -0300

    - Inserção do código da aula 18 e instruções de uso.

--- php-basic/lec-18/app/Http/Controllers/ProdutosController.php
+++ php-basic/lec-18/app/Http/Controllers/ProdutosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutosController extends Controller
 {
@@ -11,7 +12,12 @@ class ProdutosController extends Controller
         return view('produtos.create');
     }
 
-    public function store(Request $request)
+    public function show(Produto $produto)
+    {
+        return view('produtos.show', compact('produto'));
+    }
+
+    public function store(Request $request) 
     {
         $request->validate([
             'nome' => 'required',
@@ -19,6 +25,35 @@ class ProdutosController extends Controller
             'quantidade' => 'required'
         ]);
         
-        dd($request->all());
+        Produto::create([
+            'nome' => $request->nome,
+            'preco' => $request->preco,
+            'quantidade' => $request->quantidade
+        ]);
+
+        return 'Produto criado com sucesso!';
     }
+
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required',
+            'quantidade' => 'required'
+        ]);
+
+        $produto->update($request->all());
+
+        return 'Produto atualizado com sucesso!';
+    }
+
+    public function index()
+    {
+        $produtos = Produto::latest()->paginate(5);
+
+        return view('produtos.index', compact('produtos'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+
 }
