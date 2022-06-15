a3b71ad62607131bf33d81aa58053e9d8c9f5ded
Felipe <felipe.alencar@ifal.edu.br>
2022-06-15 12:30:47 -0300

    Correção de bug gitpod e reestruturação da aula 13.

--- php-basic/lec-13/excluir.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "conexao.php";
-	$id = $_POST['id'];
-	$sql = "DELETE FROM usuarios WHERE id_usuario = '$id'";
-	mysqli_query($connect, $sql);
-	$response = array("success" => true);
-	echo json_encode($response);
-?>
