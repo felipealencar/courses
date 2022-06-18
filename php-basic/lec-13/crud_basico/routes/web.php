19dd9c2bc2f9eff4eea8edd72af51c7aa745ba5a
Felipe <contato@felipealencar.net>
2022-06-18 20:46:25 +0000

    Inserção do código da aula 13.

--- php-basic/lec-13/crud_basico/routes/web.php
+++ php-basic/lec-13/crud_basico/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos/create', [ProdutosController::class, 'create']);
+
+Route::resources(['produtos' => ProdutosController::class]);
